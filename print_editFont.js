ddbab7551b934d9148205933a5fb19ff593f6354
simplearyan <aryanphone00620@gmail.com>
2026-06-29 08:25:53 +0530

    removed Patch files

--- print_editFont.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const file = path.join(__dirname, 'thumb-maker ✅', 'studio_pro.html');
-let content = fs.readFileSync(file, 'utf8');
-
-const lines = content.split('\n');
-const idx = lines.findIndex(l => l.includes('id="editFontFamily"'));
-console.log(lines.slice(idx, idx + 40).join('\n'));
